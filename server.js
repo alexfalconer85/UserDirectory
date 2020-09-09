3c198653cfcd439e7d384b14e7258314ef36f759
Alex Falconer <aef61685@gmail.com>
2020-09-08 20:43:15 -0600

    final commit?

--- server.js
+++ server.js
@@ -1,36 +1,14 @@
-// dependencies
-var express = require("express");
-var mongoose = require("mongoose");
-var path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const path = require("path");
 
-// // configure dotenv
-require("dotenv").config();
+app.use(express.static("build"));
 
-// new express app
-var app = express();
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "build/index.html"));
+});
 
-// configure middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// configure api routes
-app.use("/api", require("./routes"));
-
-// // serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
-  );
-}
-
-// // connect to database
-mongoose.connect(process.env.MONGODB_URI);
-
-// define PORT
-var PORT = process.env.PORT || 5000;
-
-// start the server
 app.listen(PORT, function () {
-  console.log("server running on port http://localhost:" + PORT);
+  console.log("App listening on PORT:", PORT);
 });
